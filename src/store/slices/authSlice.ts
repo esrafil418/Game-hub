eb9ae2f67019e5bfe564853d986baff72fe668fe
Esrafil <esrafil.s.r.man418@gmail.com>
2026-07-07 22:08:52 +0330

    feat: Add authSlice

---
+++ src/store/slices/authSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	token: sessionStorage.getItem("token") || "",
+	isAuthenticated: !!sessionStorage.getItem("token"),
+};
+
+const authSlice = createSlice({
+	name: "auth",
+	initialState,
+	reducers: {
+		setToken: (state, action) => {
+			state.token = action.payload;
+			state.isAuthenticated = !!action.payload;
+			sessionStorage.setItem("token", action.payload);
+		},
+		logout: (state) => {
+			state.token = "";
+			state.isAuthenticated = false;
+			sessionStorage.removeItem("token");
+		},
+	},
+});
+
+export const { setToken, logout } = authSlice.actions;
+export default authSlice.reducer;
