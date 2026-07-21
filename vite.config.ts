7fcef536335d65baad7cbd9493d9d441698e1ab4
Esrafil <esrafil.s.r.man418@gmail.com>
2026-07-21 22:37:07 +0330

    feat: refactor game items with shadcn components

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-	server: {
-		proxy: {
-			"/uploads": {
-				target: "http://localhost:4000",
-				changeOrigin: true,
-			},
-			"/api": {
-				target: "http://localhost:4000",
-				changeOrigin: true,
-			},
-		},
-	},
-	plugins: [tailwindcss(), react()],
+  server: {
+    proxy: {
+      "/uploads": {
+        target: "http://localhost:4000",
+        changeOrigin: true,
+      },
+      "/api": {
+        target: "http://localhost:4000",
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
